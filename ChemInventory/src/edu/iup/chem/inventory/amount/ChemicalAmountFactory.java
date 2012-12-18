bd06171504d1a8b74190a43f32eb75be8849542f
Zachary Seymour <adamsey16@gmail.com>
2012-12-18 10:27:10 -0500

    Added parsing for gallons, ounces, pounds, and fluid ounces.

--- ChemInventory/src/edu/iup/chem/inventory/amount/ChemicalAmountFactory.java
+++ ChemInventory/src/edu/iup/chem/inventory/amount/ChemicalAmountFactory.java
@@ -1,6 +1,10 @@
 package edu.iup.chem.inventory.amount;
 
+import static javax.measure.unit.NonSI.GALLON_LIQUID_US;
 import static javax.measure.unit.NonSI.LITER;
+import static javax.measure.unit.NonSI.OUNCE;
+import static javax.measure.unit.NonSI.OUNCE_LIQUID_US;
+import static javax.measure.unit.NonSI.POUND;
 import static javax.measure.unit.SI.GRAM;
 import static javax.measure.unit.SI.KILOGRAM;
 import static javax.measure.unit.SI.MILLI;
@@ -31,6 +35,24 @@ public class ChemicalAmountFactory {
 			case "liter":
 			case "liters":
 				return new ChemicalVolume(quantity, unit, LITER);
+			case "pound":
+			case "pounds":
+			case "lb":
+			case "lbs":
+				return new ChemicalMass(quantity, unit, POUND);
+			case "oz":
+			case "ounce":
+			case "ounces":
+				return new ChemicalMass(quantity, unit, OUNCE);
+			case "gal":
+			case "gals":
+			case "gallon":
+			case "gallons":
+				return new ChemicalVolume(quantity, unit, GALLON_LIQUID_US);
+			case "fl. oz":
+			case "fluid ounce":
+			case "fluid ounces":
+				return new ChemicalVolume(quantity, unit, OUNCE_LIQUID_US);
 			default:
 				return null;
 		}
