6c93354e9d5e134eef3458f8cc3a164c29b8242b
Zachary Seymour <adamsey16@gmail.com>
2012-12-19 11:37:48 -0500

    Testing densities

--- ChemInventory/src/edu/iup/chem/inventory/Test.java
+++ ChemInventory/src/edu/iup/chem/inventory/Test.java
@@ -1,6 +1,10 @@
 package edu.iup.chem.inventory;
 
 import static edu.iup.chem.inventory.db.inventory.Tables.CHEMICAL;
+import static edu.iup.chem.inventory.db.inventory.Tables.LOCATION;
+import static javax.measure.unit.NonSI.LITER;
+import static javax.measure.unit.SI.GRAM;
+import static javax.measure.unit.SI.MILLI;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -8,9 +12,13 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 
-import edu.iup.chem.inventory.dao.ChemicalDao;
+import edu.iup.chem.inventory.amount.ChemicalAmount;
+import edu.iup.chem.inventory.amount.ChemicalDensity;
+import edu.iup.chem.inventory.amount.ChemicalMass;
+import edu.iup.chem.inventory.amount.ChemicalVolume;
 import edu.iup.chem.inventory.db.inventory.InventoryFactory;
 import edu.iup.chem.inventory.db.inventory.tables.records.ChemicalRecord;
+import edu.iup.chem.inventory.db.inventory.tables.records.LocationRecord;
 
 public class Test {
 	final static Logger	log	= Logger.getLogger(Test.class);
@@ -23,10 +31,25 @@ public class Test {
 		try (Connection conn = ConnectionPool.getConnection()) {
 			final InventoryFactory create = new InventoryFactory(conn);
 			final List<ChemicalRecord> chemicals = create.select()
-					.from(CHEMICAL).fetch().into(ChemicalRecord.class);
-			for (final ChemicalRecord c : chemicals) {
-				ChemicalDao.storeNames(c);
+					.from(CHEMICAL).where(CHEMICAL.CAS.eq("100-41-4")).fetch()
+					.into(ChemicalRecord.class);
+			final ChemicalRecord r = chemicals.get(0);
+
+			final LocationRecord i = create.select().from(LOCATION)
+					.where(LOCATION.CAS.eq(r.getCas())).fetchOne()
+					.into(LocationRecord.class);
+			final ChemicalAmount am = i.getChemicalAmount();
+			final ChemicalDensity den = r.getDensity();
+
+			if (am instanceof ChemicalMass) {
+				log.debug("Volume: "
+						+ am.getAmount().divide(den.getAmount())
+								.to(MILLI(LITER)));
+			} else if (am instanceof ChemicalVolume) {
+				log.debug("Mass: "
+						+ am.getAmount().times(den.getAmount()).to(GRAM));
 			}
+
 		} catch (final SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
